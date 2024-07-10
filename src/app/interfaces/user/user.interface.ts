8dffb0e6c1e58c6a0e6c34db7256006b5e56ea3c
benfic4rthur <arthur_benfica@hotmail.com>
2024-07-10 17:25:05 -0300

    criados os estilos, as interfaces e a lista de usuarios.

---
+++ src/app/interfaces/user/user.interface.ts
@@ -0,0 +1,15 @@
+import { IStatus } from './status.interface';
+import { IAddress } from './adress.interface';
+
+export interface IUser {
+  nome: string;
+  email: string;
+  senha: string;
+  idade: number;
+  endereco: IAddress;
+  telefone: string;
+  ativo: boolean;
+  funcao: string;
+  dataCadastro: string;
+  status: IStatus;
+}
